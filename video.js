f20d2f8c779f343bb1c5e0651cc6e1296e5343c5
Kevin <123kevindante@gmail.com>
2023-07-10 20:52:05 -0300

    WebVideo

---
+++ video.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      src: "/playlist_videos/img/cacho.mp4",
+      title: "Editando fotos",
+    },
+    {
+      src: "/playlist_videos/img/02.mp4",
+      title: "Live Stream",
+    },
+    {
+      src: "/playlist_videos/img/03.mp4",
+      title: "Digitando",
+    },
+    {
+      src: "/playlist_videos/img/04.mp4",
+      title: "Home office",
+    },
+    {
+      src: "/playlist_videos/img/02.mp4",
+      title: "Likes",
+    },
+  ];
---
+++ videos.js
@@ -0,0 +1,18 @@
+export default [
+    {
+      src: "/playlist_videos/img/cacho.mp4",
+      title: "Bela Cachoeira",
+    },
+    {
+      src: "/playlist_videos/img/02.mp4",
+      title: "Dia de Neve",
+    },
+    {
+      src: "/playlist_videos/img/03.mp4",
+      title: "Uma Volta pelo metro",
+    },
+    {
+      src: "/playlist_videos/img/04.mp4",
+      title: "Acampamento",
+    },
+  ];
